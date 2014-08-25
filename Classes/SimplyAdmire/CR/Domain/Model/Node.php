02b86727cc75c6416eef9926d55f240d44cc8663
Rens Admiraal <rens@rensadmiraal.nl>
2014-08-25 12:08:36 +0200

    [TASK] Add empty createNode method to Node object

--- Classes/SimplyAdmire/CR/Domain/Model/Node.php
+++ Classes/SimplyAdmire/CR/Domain/Model/Node.php
@@ -1,6 +1,24 @@
 <?php
 namespace SimplyAdmire\CR\Domain\Model;
 
+use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
+use TYPO3\TYPO3CR\Domain\Model\NodeType;
+
 class Node extends \TYPO3\TYPO3CR\Domain\Model\Node {
+
+	/**
+	 * Creates, adds and returns a child node of this node. Also sets default
+	 * properties and creates default subnodes.
+	 *
+	 * @param string $name Name of the new node
+	 * @param NodeType $nodeType Node type of the new node (optional)
+	 * @param string $identifier The identifier of the node, unique within the workspace, optional(!)
+	 * @param array $dimensions Content dimension values to set on the node (Array of dimension names to array of values)
+	 * @param array $properties
+	 * @return NodeInterface
+	 */
+	public function createNode($name, NodeType $nodeType = NULL, $identifier = NULL, array $dimensions = NULL, array $properties = array()) {
+		
+	}
 
 }
